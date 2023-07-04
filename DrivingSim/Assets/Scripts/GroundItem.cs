2c2a2a5d793376494367cd23ad023e6ceb5cc8ed
TogglezOn <Stephenflemingbus100@gmail.com>
2023-07-04 18:51:46 +0100

    Multiplayer Tested
    Current video is character selection in multiplayer by Bobsi

---
+++ DrivingSim/Assets/Scripts/GroundItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FishNet.Object;
+ 
+//This script is made by Bobsi Unity for Youtube
+public class GroundItem : NetworkBehaviour
+{
+    public Item itemScriptable;
+}
